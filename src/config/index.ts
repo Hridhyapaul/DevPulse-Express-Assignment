7e746e4f9cd0157640d8b126ec27ab387a027948
Hridhyapaul <hpdesign2021@gmail.com>
2026-05-30 02:51:21 +0600

    Done initial Setup

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+
+export default {
+  port: process.env.PORT,
+//   connectionString: process.env.CONNECTION_STRING,
+//   secretKey: process.env.JWT_SECRET_KEY,
+};
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app";
+import config from "./config";
+// import { initDB } from "./db";
+
+const port = config.port;
+
+const main = async () => {
+  try {
+    // await initDB();
+
+    app.listen(port, () => {
+      console.log(`DevPulse Server running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+main();
